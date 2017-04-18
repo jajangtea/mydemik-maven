d8bde31e0f9e4b187941b066b34c968c75a6b6b4
User <User@User-PC>
2017-04-18 10:53:31 -0300

    update dari komputer kampus - ubah icon

--- src/main/java/util/mydemik/com/Fungsi.java
+++ src/main/java/util/mydemik/com/Fungsi.java
@@ -194,6 +194,43 @@ public class Fungsi {
         return rom;
     }
     
+    public String hurufbulan(int bln)
+    {
+       int bulan = bln;
+       String namaBulan;
+        switch(bulan)
+        {
+            case 1:  namaBulan="Januari";
+                     break;
+            case 2:  namaBulan="Februari";
+                     break;
+            case 3:  namaBulan="Maret";
+                     break;
+            case 4:  namaBulan="April";
+                     break;
+            case 5:  namaBulan="Mei";
+                     break;
+            case 6:  namaBulan="Juni";
+                     break;
+            case 7:  namaBulan="Juli";
+                     break;
+            case 8:  namaBulan="Agustus";
+                     break;
+            case 9:  namaBulan="September";
+                     break;
+            case 10: namaBulan="Oktober";
+                     break;
+            case 11: namaBulan="November";
+                     break;
+            case 12: namaBulan="Desember";
+                     break;
+            default: namaBulan = "Bulan Salah";
+            break;
+        }
+        
+        return namaBulan;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Fungsi f=new Fungsi();
